556b712cdd4e7e34e0e0c4e59216dc87cbad4359
Krishanu <krishanu.info@gmail.com>
2022-11-10 07:09:07 +0530

    update

---
+++ src/Http/Livewire/Table.php
@@ -0,0 +1,17 @@
+<?php
+namespace Mintreu\LaravelTable\Http\Livewire;
+
+
+use Livewire\Component;
+
+class Table extends Component
+{
+
+
+
+    public function render()
+    {
+        return view('table::livewire.table');
+    }
+
+}
